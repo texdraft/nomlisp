965c0757b6614877e39c7f9c87639d00e4356125
Asher Olsen <asher.r.olsen@gmail.com>
2025-10-16 09:56:06 +1100

    make environments better

--- environments.rkt
+++ environments.rkt
@@ -11,6 +11,7 @@
 
 ;; Expansion
 
+;; represents “syntactic namespace”
 (struct Synspace
   (name))
 
@@ -33,26 +34,34 @@
 (struct Syntactic-Environment
   (frames)) ; list of map of string → (map of Synspace → vector of meanings)
 
+;; make new empty frame
 (define (new-frame)
   (make-hash))
 
+;; make new empty map of synspaces to phase vectors
 (define (new-synspace-map)
   (make-hasheq))
 
+;; make new empty phase vector, up to given phase
 (define (new-phase-vector [phases 1])
-  (make-vector phases))
+  (make-vector phases #f))
 
+;; most recent frame of syntactic environment
 (define (most-recent-frame se)
   (car (Syntactic-Environment-frames se)))
 
+;; new syntactic environment with additional empty frame
 (define (add-frame se)
   (Syntactic-Environment (cons (new-frame) (Syntactic-Environment-frames se))))
 
+;; get synspace map for a name in a frame or add and return an empty one
 (define (frame-get-synspace-map! frame name)
   (hash-ref! frame
              name
              (new-synspace-map)))
 
+;; get phase vector for a synspace of a name in a frame or add and return
+;; a new one, initialized to have maximum-phase slots
 (define (frame-get-phases! frame name synspace [maximum-phase 0])
   (define desired-length (+ maximum-phase 1))
   (define synspace-map (frame-get-synspace-map! frame name))
@@ -63,6 +72,7 @@
          (hash-set! synspace-map synspace (vector-extend result desired-length))]
         [else result]))
 
+;; add binding to syntactic environment, given name and synspace and phase
 (define (add-syntactic-binding! name phase synspace meaning se)
   (let ([frame (most-recent-frame se)])
     (if (lookup-in-frame name phase synspace frame)
@@ -85,15 +95,21 @@
           (void)))
     (not-found name phase synspace)))
 
+;; change most recent meaning given name and synspace and phase,
+;; or add new entry
 (define (update-meaning! name phase synspace meaning se)
-  (if (lookup-syntactic name phase synspace se)
-      (for ([frame (in-frames se)])
-        3)
-      (add-syntactic-binding! name phase synspace se)))
+  (if (lookup-syntactic name phase synspace se (const #f))
+      (let/ec found
+        (for ([frame (in-frames se)])
+          (vector-set! (frame-get-phases! frame name synspace phase)
+                       phase
+                       meaning)))
+      (add-syntactic-binding! name phase synspace meaning se))
+  meaning)
 
+;; iteration abstractions
 (define (in-frames se)
   (Syntactic-Environment-frames se))
-
 (define (in-frame frame)
   frame)
 
